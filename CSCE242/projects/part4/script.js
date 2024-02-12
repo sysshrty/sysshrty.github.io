70686759770a2a5358b7aadabda4373f9d5bfc5a
Sherry <sbegay@email.sc.edu>
2024-02-11 23:47:41 -0500

    did my best-final

--- CSCE242/projects/part4/script.js
+++ CSCE242/projects/part4/script.js
@@ -1,20 +1,12 @@
-// Toggles the nav items to show/hide when the hamburger menu is clicked
+
 
 const toggleHamburger = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
   };
-  
-  // Calls the toggleHamburger function when the hamburger menu is clicked
   window.onload = () => {
     document.getElementById("hamburger").onclick = toggleHamburger;
   };
 
-/*
-const showHideNav = () => {
-    document.getElementById("nav-items").classList.toggle("hide-small");
-};
-document.getElementById("hamburger").onclick = showHideNav;
-*/
 
 const evalForm = (e) => {
   e.percentDefault();
