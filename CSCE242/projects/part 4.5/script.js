b8309da537374b59dc01679d6e5944c21999368b
Sherry <sbegay@email.sc.edu>
2024-02-25 23:45:39 -0500

    finished prject 4.5

---
+++ CSCE242/projects/part 4.5/script.js
@@ -0,0 +1,23 @@
+const showHideNav = () => {
+  document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+  window.onload = () => {
+    document.getElementById("hamburger").onclick = toggleHamburger;
+  };
+
+
+const evalForm = (e) => {
+  e.percentDefault();
+  console.log("here");
+}
+document.getElementById("my-form").onsubmit = evalForm;
+document.getElementById("hamburger").onclick = showHideNav;
+
+const toggleHamburger = () => {
+  document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+window.onload = () => {
+  document.getElementById("hamburger").onclick = toggleHamburger;
+};
